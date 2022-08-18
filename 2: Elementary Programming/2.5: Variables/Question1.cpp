6e0fac71af3255a876b03eec98d6e0a38e5c6189
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-17 23:41:02 -0500

    Create Question1.cpp

---
+++ 2: Elementary Programming/2.5: Variables/Question1.cpp
@@ -0,0 +1,17 @@
+/*
+Write a complete program that
+declares an int variable,
+reads a value from the keyboard into that variable, and
+writes to standard output the square of the variable's value.
+Besides the number, nothing else should be written to standard output.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int amogus;
+    cin >> amogus;
+    cout << amogus * amogus;
+}
