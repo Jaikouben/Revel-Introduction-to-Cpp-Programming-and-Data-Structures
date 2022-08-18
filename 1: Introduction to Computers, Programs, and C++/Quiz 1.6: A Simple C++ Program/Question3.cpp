94dd0b691febccb1e4a7fcbe62da4583015c864f
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-17 23:26:47 -0500

    Create Question3.cpp

---
+++ 1: Introduction to Computers, Programs, and C++/Quiz 1.6: A Simple C++ Program/Question3.cpp
@@ -0,0 +1,6 @@
+/*
+Suppose your name was Alan Turing. Write a statement that would print your last name, followed by a comma, followed by your first name.
+Do not print anything else (that includes blanks).
+*/
+
+cout << "Turing,Alan";
