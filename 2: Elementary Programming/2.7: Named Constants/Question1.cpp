ff5340096a83c341ae85d51102ce6adcd80cf76d
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-17 23:51:58 -0500

    Create Question1.cpp

---
+++ 2: Elementary Programming/2.7: Named Constants/Question1.cpp
@@ -0,0 +1,6 @@
+/*
+Declare an int constant, MonthsInYear, whose value is 12.
+Note: The quizzes are a third-party product. The name for the constant does not follow the convention used in the text. It would be named MONTHS_IN_YEAR if the convention in the text is used. This is the only place we alert our users. We will not explicitly point it out in other places.
+*/
+
+const int MonthsInYear = 12;
