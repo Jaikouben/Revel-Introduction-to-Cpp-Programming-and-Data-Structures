0ab9eb2a6446897647d703f505093726da208ba6
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-18 00:08:25 -0500

    Create Question3.cpp

---
+++ 2: Elementary Programming/2.8: Numeric Data Types and Operations/Question3.cpp
@@ -0,0 +1,6 @@
+/*
+Assume two variables endingTime and startingTime are already declared and assigned values.
+Write an expression that computes the difference of the variables endingTime and startingTime. 
+*/
+
+endingTime - startingTime
