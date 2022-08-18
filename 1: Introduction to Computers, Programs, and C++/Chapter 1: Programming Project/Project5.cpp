555c6e053c890b5fdf69987a43a954b352e17191
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-17 23:35:42 -0500

    Create Project5.cpp

---
+++ 1: Introduction to Computers, Programs, and C++/Chapter 1: Programming Project/Project5.cpp
@@ -0,0 +1,14 @@
+/*
+(Physics: acceleration)
+Average acceleration is defined as the change of velocity divided by the time taken to make the change, as shown in the following formula:
+a = (v1 - v0) / t
+Here, v0 is the starting velocity in meters/second, v1 is the ending velocity in meters/second, and t is the time span in seconds. Assume v0 is 5.6, v1 is 10.5, and t is 0.5, and write the code to display the average acceleration. 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << (10.5 - 5.6) / 0.5;
+}
