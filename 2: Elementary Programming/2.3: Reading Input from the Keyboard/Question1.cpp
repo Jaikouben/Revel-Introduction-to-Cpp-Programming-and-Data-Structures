7631a2f6b70fe6a49321d95ad5055d5118657097
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-17 23:39:27 -0500

    Create Question1.cpp

---
+++ 2: Elementary Programming/2.3: Reading Input from the Keyboard/Question1.cpp
@@ -0,0 +1,6 @@
+/*
+Write a statement that reads a floating point (real) value from standard input into temperature.
+Assume that temperature has already been declared as a double variable.
+*/
+
+cin >> temperature;
