91ad6f825cc771aeb341ce336f091feae4c2f7bb
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-17 23:31:45 -0500

    Create Question1.cpp

---
+++ 1: Introduction to Computers, Programs, and C++/Chapter 1: Programming Project/Question1.cpp
@@ -0,0 +1,18 @@
+/*
+(Display three messages)
+Write a program that displays
+Welcome to C++
+Welcome to Computer Science
+Programming is fun
+*/
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    cout << "Welcome to C++" << endl;
+    cout << "Welcome to Computer Science" << endl;
+    cout << "Programming is fun" << endl;
+    return 0;
+}
