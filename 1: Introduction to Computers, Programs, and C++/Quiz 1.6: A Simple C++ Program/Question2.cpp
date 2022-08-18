8f1f41a9b6731e21202d8ddb70103e9502ae0246
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-17 23:26:11 -0500

    Create Question2.cpp

---
+++ 1: Introduction to Computers, Programs, and C++/Quiz 1.6: A Simple C++ Program/Question2.cpp
@@ -0,0 +1,13 @@
+/*
+Write a complete program that prints Hello World to the screen.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Displays Hello World
+    cout << "Hello World" << endl;
+    return 0;
+}
