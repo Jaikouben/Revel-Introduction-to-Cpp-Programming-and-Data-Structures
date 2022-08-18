023d2d3688153ef37a9b8eefde8b1fff96a8a4f2
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-17 23:52:37 -0500

    Create Question2.cpp

---
+++ 2: Elementary Programming/2.7: Named Constants/Question2.cpp
@@ -0,0 +1,5 @@
+/*
+Declare an int constant MonthsInDecade whose value is the value of the constant MonthsInYear (already declared) multiplied by 10.
+*/
+
+const int MonthsInDecade = MonthsInYear * 10;
