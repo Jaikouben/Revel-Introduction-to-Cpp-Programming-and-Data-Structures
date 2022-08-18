648cbf12c9ca60a67df4c298d3dcfb70a091095e
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-17 23:33:48 -0500

    Create Project4.cpp

---
+++ 1: Introduction to Computers, Programs, and C++/Chapter 1: Programming Project/Project4.cpp
@@ -0,0 +1,12 @@
+/*
+(Simple computation)
+The formula for computing the discriminant of a quadratic equation ax^2 + bx  + c = 0 is b^2 – 4ac. Write a program that computes the discriminant for the equation 3x^2 + 4x + 5 = 0.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << 4 * 4 - 4 * 3 * 5;
+}
