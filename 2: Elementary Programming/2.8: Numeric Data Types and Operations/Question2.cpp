ec59c9f53fc39b3631db3565796c457f4fe26fd8
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-18 00:06:36 -0500

    Create Question2.cpp

---
+++ 2: Elementary Programming/2.8: Numeric Data Types and Operations/Question2.cpp
@@ -0,0 +1,6 @@
+/*
+Assume that variables verbalScore and mathScore are already declared and assigned values.
+Write an expression that computes the sum of the two variables verbalScore and mathScore.
+*/
+
+verbalScore + mathScore
