bc3fabe6689640fa35a168ab6344cc945c134f8b
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-09-23 15:57:12 -0500

    Create Question3.cpp

---
+++ 3: Selections/3.10: Logical Operators/Question3.cpp
@@ -0,0 +1,5 @@
+/*
+Given the bool variable isFullTimeStudent and the int variable age, write an expression that evaluates to true if age is less than 19 or isFullTimeStudent is true.
+*/
+
+isFullTimeStudent == true || age < 19
