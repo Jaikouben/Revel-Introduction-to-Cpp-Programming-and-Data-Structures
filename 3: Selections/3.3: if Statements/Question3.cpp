6358085d70292bf2037ccaf70e1d735c06e8ac6b
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-09-23 15:50:24 -0500

    Create Question3.cpp

---
+++ 3: Selections/3.3: if Statements/Question3.cpp
@@ -0,0 +1,11 @@
+/*
+Assume that the variables gpa, deansList, and studentName have been declared and initialized.
+
+Write a statement that both adds 1 to deansList and prints studentName to standard output if gpa exceeds 3.5.
+*/
+
+if (gpa > 3.5)
+{
+    deansList += 1;
+    cout << studentName;
+}
