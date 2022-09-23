232a5f9c94e2e793b79ba4e4b58118ed7110b9c8
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-09-23 15:56:49 -0500

    Create Question2.cpp

---
+++ 3: Selections/3.10: Logical Operators/Question2.cpp
@@ -0,0 +1,5 @@
+/*
+Given the int variables yearsWithCompany and department, write an expression that evaluates to true if yearsWithCompany is less than 5 and department is not equal to 99.
+*/
+
+yearsWithCompany < 5 && department != 99
