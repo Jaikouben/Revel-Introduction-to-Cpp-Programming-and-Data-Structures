f28fa1760e6eadfb8f8191260e01d672f96096a7
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-09-23 16:01:40 -0500

    Update Question2.cpp

--- 3: Selections/3.13: switch Statements/Question2.cpp
+++ 3: Selections/3.13: switch Statements/Question2.cpp
@@ -1,15 +1,36 @@
 /*
-HTTP is the protocol that governs communications between Web servers and Web clients (i.e. browsers). Part of the protocol includes a status code returned by the server to tell the browser the status of its most recent page request. Some of the codes and their meanings are listed below:
-• 200, OK (fulfilled)
-• 403, forbidden
-• 404, not found
-• 500, server error
-Given an int variable status, write a switch statement that prints out the appropriate label from the above list based on status.
+In the Happy Valley School System, children are classified by age as follows:
+• less than 2, ineligible
+• 2, toddler
+• 3-5, early childhood
+• 6-7, young reader
+• 8-10, elementary
+• 11 and 12, middle
+• 13, impossible
+• 14-16, high school
+• 17-18, scholar
+• greater than 18, ineligible
+Given an int variable age, write a switch statement that prints out the appropriate label from the above list based on age.
 */
 
-switch (status) {
-    case 200: cout << "OK (fulfilled)"; break;
-    case 403: cout << "forbidden"; break;
-    case 404: cout << "not found"; break;
-    case 500: cout << "server error"; break;
+switch (age) {
+    case 1: cout << "less than 2, ineligible"; break;
+    case 2: cout << "toddler"; break;
+    case 3: cout << "early childhood"; break;
+    case 4: cout << "early childhood"; break;
+    case 5: cout << "early childhood"; break;
+    case 6: cout << "young reader"; break;
+    case 7: cout << "young reader"; break;
+    case 8: cout << "elementary"; break;
+    case 9: cout << "elementary"; break;
+    case 10: cout << "elementary"; break;
+    case 11: cout << "middle"; break;
+    case 12: cout << "middle"; break;
+    case 13: cout << "impossible"; break;
+    case 14: cout << "high school"; break;
+    case 15: cout << "high school"; break;
+    case 16: cout << "high school"; break;
+    case 17: cout << "scholar"; break;
+    case 18: cout << "scholar"; break;
+    default: cout << "ineligible"; break;
 }
