f3eaeb667f381f8f0caf61f0dfe25b51772edb54
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-09-23 15:37:00 -0500

    Create Question2.cpp

---
+++ 3: Selections/3.2: The bool Data Type/Question2.cpp
@@ -0,0 +1,5 @@
+/*
+Given the variables numberOfMen and numberOfWomen, write an expression that evaluates to true if the number of men is greater than or equal to the number of women.
+*/
+
+numberOfMen >= numberOfWomen
