4eebead081cefc9c4c63b36c04b4dedf273e6171
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-09-23 15:59:28 -0500

    Create Question3.cpp

---
+++ 3: Selections/3.11: Case Study: Determining Leap Year/Question3.cpp
@@ -0,0 +1,7 @@
+/*
+Clunker Motors Inc. is recalling all vehicles from model years 2001-2006.
+Given a variable modelYear, write a statement that prints the message "RECALL" to standard output if the value of modelYear falls within that range.
+*/
+
+if (modelYear >= 2001 && modelYear <= 2006)
+    cout << "RECALL";
