026281d015c447496db1ed15047e0890f77bb448
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-09-23 14:53:10 -0500

    Create Question1.cpp

---
+++ 3: Selections/Quiz 3.2: The bool Data Type/Question1.cpp
@@ -0,0 +1,6 @@
+/*
+Working overtime is defined as having worked more than 40 hours during the week.
+Given the variable hoursWorked, write an expression that evaluates to true if the employee worked overtime.
+*/
+
+hoursWorked > 40
