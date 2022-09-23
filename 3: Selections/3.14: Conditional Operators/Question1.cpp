589f71a4fd54dc945160d5440457500c93ba170e
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-09-23 16:02:44 -0500

    Create Question1.cpp

---
+++ 3: Selections/3.14: Conditional Operators/Question1.cpp
@@ -0,0 +1,7 @@
+/*
+Write an expression using the conditional operator (? :) that compares the value of the variable x to 5 and results in:
+• x if x is greater than or equal to 5
+• -x if x is less than 5
+*/
+
+x >= 5 ? x : -x
