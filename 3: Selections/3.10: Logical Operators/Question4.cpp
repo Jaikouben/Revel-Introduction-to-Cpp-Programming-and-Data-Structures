38c4c9c25b151a443416a4728cc5c07eed4aedac
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-09-23 15:57:32 -0500

    Create Question4.cpp

---
+++ 3: Selections/3.10: Logical Operators/Question4.cpp
@@ -0,0 +1,10 @@
+/*
+Clunker Motors Inc. is recalling all vehicles from model years 2001-2006. A bool variable named recalled has been declared.
+Given an int variable modelYear, write a statement that assigns true to recalled if the value of modelYear falls within the recall range and assigns false otherwise.
+Do not use an if statement in this exercise!
+*/
+
+if (modelYear >= 2001 && modelYear <= 2006)
+    recalled = true;
+else
+    recalled = false;
