e2f8e8a4dad76ea1df815a1b40187ca786531264
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-09-23 15:46:47 -0500

    Create Question1.cpp

---
+++ 3: Selections/3.3: if Statements/Question1.cpp
@@ -0,0 +1,6 @@
+/*
+Write an if statement that assigns 10,000 to the variable bonus if the value of the variable goodsSold is greater than 500,000. Assume bonus and goodsSold have been declared and initialized.
+*/
+
+if (goodsSold > 500000)
+    bonus = 10000;
