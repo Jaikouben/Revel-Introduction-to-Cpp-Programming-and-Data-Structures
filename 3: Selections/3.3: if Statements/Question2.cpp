d2c84352b524300887d467733e9bfc6f75e04b4d
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-09-23 15:47:13 -0500

    Create Question2.cpp

---
+++ 3: Selections/3.3: if Statements/Question2.cpp
@@ -0,0 +1,6 @@
+/*
+Write an if statement that decreases the variable shelfLife by 4 if the variable outsideTemperature is greater than 90. Assume shelfLife and outsideTemperature have been declared and initialized.
+*/
+
+if (outsideTemperature > 90)
+    shelfLife -= 4;
