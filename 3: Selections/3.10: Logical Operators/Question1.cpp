80339f81545a875510fc116021f3a6ddc151a2f4
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-09-23 15:56:26 -0500

    Create Question1.cpp

---
+++ 3: Selections/3.10: Logical Operators/Question1.cpp
@@ -0,0 +1,5 @@
+/*
+Given the variables temperature and humidity, write an expression that evaluates to true if and only if the temperature is greater than 90 and the humidity is less than 10.
+*/
+
+temperature > 90 && humidity < 10
