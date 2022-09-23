3481d4580c90ca00c0fbd910f9666a3b39fcffff
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-09-23 14:54:13 -0500

    Create Question3.cpp

---
+++ 3: Selections/Quiz 3.2: The bool Data Type/Question3.cpp
@@ -0,0 +1,5 @@
+/*
+Write an expression that evaluates to true if the value of the int variable x is divisible (with no remainder) by the int variable y. (Assume that y is not zero.) All the variables used in the programming quizzes are assumed to be declared already unless explicitly specified otherwise. This is the only place we alert our users. We will not explicitly point it out in other places.
+*/
+
+x % y == 0
