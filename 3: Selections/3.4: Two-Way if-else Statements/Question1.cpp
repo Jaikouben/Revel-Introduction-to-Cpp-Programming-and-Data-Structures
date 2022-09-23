88c83577eb362f1daff3115dd5c582c58e067cd5
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-09-23 15:51:55 -0500

    Create Question1.cpp

---
+++ 3: Selections/3.4: Two-Way if-else Statements/Question1.cpp
@@ -0,0 +1,8 @@
+/*
+Write an if/else statement that compares the variable age with 65, adds 1 to the variable seniorCitizens if age is greater than or equal to 65, and adds 1 to the variable nonSeniors otherwise. Assume all variables have been declared and initialized.
+*/
+
+if (age >= 65)
+    seniorCitizens += 1;
+else
+    nonSeniors += 1;
