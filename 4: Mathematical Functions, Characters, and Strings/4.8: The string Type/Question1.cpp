6456ae39f0e12ea18f6ec603a3c8116a3b120732
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-03 17:45:29 -0500

    Create Question1.cpp

---
+++ 4: Mathematical Functions, Characters, and Strings/4.8: The string Type/Question1.cpp
@@ -0,0 +1,5 @@
+/*
+Given a string variable address, write a string expression consisting of the string "http://" concatenated with the variable's string value. So, if the value of the variable were "www.turingscraft.com", the value of the expression would be "http://www.turingscraft.com".
+*/
+
+"http://" + address
