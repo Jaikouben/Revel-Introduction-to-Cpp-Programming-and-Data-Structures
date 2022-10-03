07750414ef9142438c31907c1ab88c3f06af7d87
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-03 17:54:03 -0500

    Create Question3.cpp

---
+++ 4: Mathematical Functions, Characters, and Strings/4.11: Simple File Input and Output/Question3.cpp
@@ -0,0 +1,5 @@
+/*
+Given that corpdata is an ifstream object that has been used for reading data and that there is no more data to be read, write the necessary code to complete your use of this object.
+*/
+
+corpdata.close();
