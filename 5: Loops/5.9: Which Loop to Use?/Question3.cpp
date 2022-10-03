cc61d01cd54944d5871cd3a170b3cc06d1d1ec26
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-03 18:12:48 -0500

    Create Question3.cpp

---
+++ 5: Loops/5.9: Which Loop to Use?/Question3.cpp
@@ -0,0 +1,9 @@
+/*
+Write a for loop that prints the odd integers 11 through 121 inclusive, separated by spaces.
+*/
+
+for (int i = 11; i <= 121; i++)
+{
+    if (i % 2 != 0)
+        cout << i << " ";
+}
