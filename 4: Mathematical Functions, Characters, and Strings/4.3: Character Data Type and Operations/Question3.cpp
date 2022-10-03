8fc359be876b985d7376268d181e01751740ea0d
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-03 17:42:40 -0500

    Create Question3.cpp

---
+++ 4: Mathematical Functions, Characters, and Strings/4.3: Character Data Type and Operations/Question3.cpp
@@ -0,0 +1,5 @@
+/*
+Assume that x is a char variable that has been declared and already given a value. Write an expression whose value is true if and only if x is a decimal digit (0-9).
+*/
+
+(x >= '0' && x <= '9')
