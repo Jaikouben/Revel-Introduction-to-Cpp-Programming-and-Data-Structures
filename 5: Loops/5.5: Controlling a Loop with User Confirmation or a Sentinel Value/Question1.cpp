4da438ae33acaa709c60e2a41b37342f94d1437c
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-03 18:07:30 -0500

    Create Question1.cpp

---
+++ 5: Loops/5.5: Controlling a Loop with User Confirmation or a Sentinel Value/Question1.cpp
@@ -0,0 +1,8 @@
+/*
+Given a char variable c that has already been declared, write some code that repeatedly reads a value from console input into c until at last a 'Y' or 'y' or 'N' or 'n' has been entered.
+*/
+
+while (c != 'Y' && c != 'y' && c != 'N' && c != 'n')
+{
+    cin >> c;
+}
