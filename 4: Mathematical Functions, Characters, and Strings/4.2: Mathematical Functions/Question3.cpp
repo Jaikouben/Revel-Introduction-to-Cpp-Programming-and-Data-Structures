3be8b4d78ca74a80007254754a3f1e2ee9d63d54
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-03 17:39:09 -0500

    Create Question3.cpp

---
+++ 4: Mathematical Functions, Characters, and Strings/4.2: Mathematical Functions/Question3.cpp
@@ -0,0 +1,6 @@
+/*
+If a right triangle has sides of length a, b and c and if c is the largest, then it is called the "hypotenuse" and its length is the square root of the sum of the squares of the lengths of the shorter sides (a and b).
+Assume that variables a and b have been declared as double and that a and b contain the lengths of the shorter sides of a right triangle. Write an expression for the length of the hypotenuse.
+*/
+
+sqrt((pow(a, 2)) + (pow(b,2)))
