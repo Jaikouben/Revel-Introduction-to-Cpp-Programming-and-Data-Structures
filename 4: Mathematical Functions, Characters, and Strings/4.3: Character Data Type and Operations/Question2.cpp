656487b1e6a2e699dded10526cdcc781658f0731
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-03 17:42:20 -0500

    Create Question2.cpp

---
+++ 4: Mathematical Functions, Characters, and Strings/4.3: Character Data Type and Operations/Question2.cpp
@@ -0,0 +1,5 @@
+/*
+Assume that x is a char variable that has been declared and already given a value. Write an expression whose value is true if and only if x is a lowercase letter.
+*/
+
+(x >= 'a' && x <= 'z')
