427b49da4f33b2162118fb80b47fde6b6c23d9be
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-03 18:02:48 -0500

    Create Question2.cpp

---
+++ 5: Loops/5.2: The while Loop/Question2.cpp
@@ -0,0 +1,8 @@
+/*
+Given an int variable n that has already been declared, write some code that repeatedly reads a value into n until at last a number between 1 and 10 (inclusive) has been entered. Don't forget to initialize n with an appropriate value. 
+*/
+
+while (n > 10 || n < 1)
+{
+    cin >> n;
+}
