0ab922b15d5ce2d40412b11885907727ed101fc5
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-03 17:43:09 -0500

    Create Question4.cpp

---
+++ 4: Mathematical Functions, Characters, and Strings/4.3: Character Data Type and Operations/Question4.cpp
@@ -0,0 +1,5 @@
+/*
+Assume that x is a char variable that has been declared and already given a value. Write an expression whose value is true if and only if x is a letter.
+*/
+
+(x >= 'A' && x <= 'Z') || (x >= 'a' && x <= 'z')
