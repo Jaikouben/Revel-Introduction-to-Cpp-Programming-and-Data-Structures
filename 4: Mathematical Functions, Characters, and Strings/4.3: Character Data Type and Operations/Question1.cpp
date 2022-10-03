e5a4adb041406490767d1bb07e77df7477f6eecc
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-03 17:41:56 -0500

    Create Question1.cpp

---
+++ 4: Mathematical Functions, Characters, and Strings/4.3: Character Data Type and Operations/Question1.cpp
@@ -0,0 +1,5 @@
+/*
+Assume that x is a char variable that has been declared and already given a value. Write an expression whose value is true if and only if x is an uppercase letter.
+*/
+
+(x >= 'A' && x <= 'Z')
