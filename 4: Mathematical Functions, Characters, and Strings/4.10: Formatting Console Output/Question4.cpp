63034ef582959e2fe9edc68d96cbfe8f78de259d
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-03 17:51:41 -0500

    Create Question4.cpp

---
+++ 4: Mathematical Functions, Characters, and Strings/4.10: Formatting Console Output/Question4.cpp
@@ -0,0 +1,5 @@
+/*
+Assume that x is a double variable that has been initialized. Write a statement that prints it out, guaranteed to have a decimal point, but without forcing scientific (also known as exponential or e-notation).
+*/
+
+cout << showpoint << x;
