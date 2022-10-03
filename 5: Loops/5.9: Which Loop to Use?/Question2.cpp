8f0ecab35465e392d0ad0cbba883ccec381dfb32
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-03 18:12:26 -0500

    Create Question2.cpp

---
+++ 5: Loops/5.9: Which Loop to Use?/Question2.cpp
@@ -0,0 +1,7 @@
+/*
+Given an int variable i that has already been declared, write a for loop that prints the integers 0 through 39, separated by spaces.
+Use no variables other than i.
+*/
+
+for (i = 0; i <= 39; i++)
+    cout << i << " ";
