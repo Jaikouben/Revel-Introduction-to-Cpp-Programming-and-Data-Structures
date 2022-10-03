6b5d89206f13a35386793343ce9f351c4d933a39
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-03 18:10:31 -0500

    Create Question1.cpp

---
+++ 5: Loops/5.8: The for Loop/Question1.cpp
@@ -0,0 +1,7 @@
+/*
+Given an int variable k that has already been declared, use a for loop to print a single line consisting of 97 asterisks.
+Use no variables other than k.
+*/
+
+for (k = 0; k < 97; k++)
+    cout << "*";
