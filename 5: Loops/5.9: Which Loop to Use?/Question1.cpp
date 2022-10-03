018c599a3a782ed0cd4757caeb23c09cc7aa3bd4
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-03 18:12:00 -0500

    Create Question1.cpp

---
+++ 5: Loops/5.9: Which Loop to Use?/Question1.cpp
@@ -0,0 +1,6 @@
+/*
+Write a for loop that prints the integers 1 through 40, separated by spaces or new lines. You may use only one variable, count, which has already been declared as an int.
+*/
+
+for (count = 1; count <= 40; count++)
+    cout << count << endl;
