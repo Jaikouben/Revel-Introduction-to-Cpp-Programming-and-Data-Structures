b56e04c8fcaef1994bc0b6d70be6191f8bbeb692
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-03 18:09:14 -0500

    Create Question1.cpp

---
+++ 5: Loops/5.7: The do-while Loop/Question1.cpp
@@ -0,0 +1,11 @@
+/*
+Given an int variable k that has already been declared, use a do-while loop to print a single line consisting of 97 asterisks.
+Use no variables other than k. Don’t forget to initialize k with an appropriate value.
+*/
+
+k = 0;
+do 
+{
+    cout << "*";
+    k++;
+} while (k < 97);
