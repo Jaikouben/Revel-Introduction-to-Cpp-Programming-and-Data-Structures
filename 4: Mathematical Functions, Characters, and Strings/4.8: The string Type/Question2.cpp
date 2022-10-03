9fb6115bc4485527e473114a042e3d44bce03681
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-03 17:46:38 -0500

    Create Question2.cpp

---
+++ 4: Mathematical Functions, Characters, and Strings/4.8: The string Type/Question2.cpp
@@ -0,0 +1,7 @@
+/*
+Clunker Motors Inc. is recalling all vehicles in its Extravagant line from model years 1999-2002.
+Given an int variable modelYear and a string modelName, write a statement that prints the message "RECALL" to standard output if the values of modelYear and modelName match the recall details.
+*/
+
+if (modelName == "Extravagant" && modelYear >= 1999 && modelYear <= 2002)
+    cout << "RECALL";
