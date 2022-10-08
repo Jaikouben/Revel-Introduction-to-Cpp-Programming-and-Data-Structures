82a345a6c15c680195970d051d8afab30b2a3949
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-08 14:42:20 -0500

    Create Question1.cpp

---
+++ 7: Single-Dimensional Arrays and C-Strings/7.2: Array Basics/Question1.cpp
@@ -0,0 +1,9 @@
+/*
+We informally define the term "corresponding element" as follows:
+The first element in an array and the last element of the array are corresponding elements.
+Similarly, the second element and the second last element are corresponding elements.
+The third element and the third last element are corresponding elements -- and so on.
+Given an array a and a variable n that contains the number of elements in a, write an expression for the corresponding element of a[i].
+*/
+
+a[n - 1 - i]
