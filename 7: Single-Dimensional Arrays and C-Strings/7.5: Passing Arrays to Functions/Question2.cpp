25dfa4b020aaf57b45ab8910412132ce854b1aa7
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-08 14:44:50 -0500

    Create Question2.cpp

---
+++ 7: Single-Dimensional Arrays and C-Strings/7.5: Passing Arrays to Functions/Question2.cpp
@@ -0,0 +1,7 @@
+/*
+Write a statement that declares a prototype for a function printArray, which has two parameters.
+The first parameter is an array of element type int and the second is an int, the number of elements in the array.
+The function does not return a value.
+*/
+
+void printArray (int [], int );
