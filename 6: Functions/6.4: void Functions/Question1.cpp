121c42262db0c4dbe589b3499a86d67fadc02ff7
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-08 13:38:22 -0500

    Create Question1.cpp

---
+++ 6: Functions/6.4: void Functions/Question1.cpp
@@ -0,0 +1,6 @@
+/*
+printErrorDescription is a function that accepts one int parameter and returns no value.
+Write a statement that calls the function printErrorDescription, passing it the value 14.
+*/
+
+printErrorDescription(14);
