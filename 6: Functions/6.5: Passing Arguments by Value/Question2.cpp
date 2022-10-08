e918e658792436fa299f02ab761fb20dc9a8073c
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-08 13:41:30 -0500

    Create Question2.cpp

---
+++ 6: Functions/6.5: Passing Arguments by Value/Question2.cpp
@@ -0,0 +1,7 @@
+/*
+add is a function that accepts two int parameters and returns their sum.
+Two int variables, euroSales and asiaSales, have already been declared and initialized. Another int variable, eurasiaSales, has already been declared.
+Write a statement that calls add to compute the sum of euroSales and asiaSales and stores this value in eurasiaSales.
+*/
+
+eurasiaSales = add(euroSales, asiaSales);
