3f4faa703c3be95eba4e812f52898ffeebe02d80
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-08 13:42:52 -0500

    Create Question1.cpp

---
+++ 6: Functions/6.7: Overloading Functions/Question1.cpp
@@ -0,0 +1,29 @@
+/*
+Write the definitions for three functions named max.
+Each receives two parameters of the same type and returns the larger of the two values.
+Define one of these functions to apply to type double, another to type int, and a third to type char.
+*/
+
+double max(double a, double b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+int max(int a, int b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+char max(char a, char b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
