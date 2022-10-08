37ba9ad446bc8f03b150d6724baea7ccb225a813
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-08 13:38:54 -0500

    Create Question2.cpp

---
+++ 6: Functions/6.4: void Functions/Question2.cpp
@@ -0,0 +1,9 @@
+/*
+Write the definition of a function printDottedLine, which has no parameters and doesn't return anything.
+The function prints a single line consisting of 5 periods (terminated by a new line character) .
+*/
+
+void printDottedLine()
+{
+    cout << "....." << endl;
+}
