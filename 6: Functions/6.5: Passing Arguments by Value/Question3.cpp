96146f9bee82ab4f198215125259d6d5ef2d03cc
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-08 13:41:55 -0500

    Create Question3.cpp

---
+++ 6: Functions/6.5: Passing Arguments by Value/Question3.cpp
@@ -0,0 +1,7 @@
+/*
+toThePowerOf is a function that accepts two int parameters and returns the value of the first parameter raised to the power of the second.
+An int variable cubeSide has already been declared and initialized. Another int variable, cubeVolume, has already been declared.
+Write a statement that calls toThePowerOf to compute the value of cubeSide raised to the power of 3 and stores this value in cubeVolume.
+*/
+
+cubeVolume = toThePowerOf(cubeSide, 3);
