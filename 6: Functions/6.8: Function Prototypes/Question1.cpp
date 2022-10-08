1350e8f570ef4f33b68808511e8217411be2bf02
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-08 13:43:55 -0500

    Create Question1.cpp

---
+++ 6: Functions/6.8: Function Prototypes/Question1.cpp
@@ -0,0 +1,6 @@
+/*
+Write a statement that declares a prototype for a function powerTo, which has two parameters.
+The first is a double and the second is an int. The function returns a double.
+*/
+
+double powerTo(double a, int b);
