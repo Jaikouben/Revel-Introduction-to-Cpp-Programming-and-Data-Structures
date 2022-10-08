7c95e30416233a34265c376724445e7933d5db6e
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-08 13:36:46 -0500

    Create Question1.cpp

---
+++ 6: Functions/6.3: Calling a Function/Question1.cpp
@@ -0,0 +1,9 @@
+/*
+Write the definition of a function twice, that receives an int parameter and returns an int that is twice the value of that parameter.
+*/
+
+int twice(int a)
+{
+    a = a * 2;
+    return a;
+}
