4f723b6e79566f179bd829ab7042789fb008677c
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-08 14:44:22 -0500

    Create Question1.cpp

---
+++ 7: Single-Dimensional Arrays and C-Strings/7.5: Passing Arrays to Functions/Question1.cpp
@@ -0,0 +1,7 @@
+/*
+printArray is a function that has two parameters. The first parameter is an array of element type int and the second is an int, the number of elements in the array. The function prints the contents of the array parameter; it does not return a value.
+inventory is an array of ints that has been already declared and filled with values. n is an int variable that holds the number of elements in the array.
+Write a statement that prints the contents of the array inventory by calling the function printArray.
+*/
+
+printArray(inventory, n);
