39185167af1c4febf0e8dcb22a92336461a079a0
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-08 13:40:27 -0500

    Delete Question4.cpp

--- 6: Functions/6.4: void Functions/Question4.cpp
+++
@@ -1,6 +0,0 @@
-/*
-Assume that printStars is a function that takes one argument and returns no value. It prints a line of n stars (followed by a newline character ) where n is the value of the argument received.
-Write a statement that invokes printStars to make a line of 35 stars.
-*/
-
-printStars(35);
