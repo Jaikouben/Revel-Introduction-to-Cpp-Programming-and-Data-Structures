51bbaee696ee4db2629dc2362e45b0d3699b1d99
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-08 14:48:25 -0500

    Create Question1.cpp

---
+++ 7: Single-Dimensional Arrays and C-Strings/7.11: C-Strings/Question1.cpp
@@ -0,0 +1,6 @@
+/*
+Declare a char array named line of size 50, and write a statement that reads in the next line of console input into this array. (Assume the line may contain whitespace characters and the total number of the characters in the line is less than 50)
+*/
+
+char line[50];
+cin.get(line, 50);
