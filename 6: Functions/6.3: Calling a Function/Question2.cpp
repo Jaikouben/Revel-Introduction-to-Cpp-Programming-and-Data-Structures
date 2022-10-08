10a6a859c3bfd2e8a6bb091aec66952149bc8a17
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-08 13:37:14 -0500

    Create Question2.cpp

---
+++ 6: Functions/6.3: Calling a Function/Question2.cpp
@@ -0,0 +1,8 @@
+/*
+Write the definition of a function oneLess, which receives an int parameter and returns an int that is one less than the value of the parameter. So if the parameter's value is 7, the function returns the value 6. If the parameter's value happens to be 44, the functions returns the value 43.
+*/
+
+int oneLess(int a)
+{
+    return a - 1;
+}
