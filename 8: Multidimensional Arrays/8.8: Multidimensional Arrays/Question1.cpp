ee61794354e7a2127727d62c72ef6c33108f5b26
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-10-12 13:47:06 -0500

    Create Question1.cpp

---
+++ 8: Multidimensional Arrays/8.8: Multidimensional Arrays/Question1.cpp
@@ -0,0 +1,10 @@
+/*
+Declare an array of int values westboundHollandTunnelTraffic that can store the number of vehicles going westbound through the Holland Tunnel
+on a particular hour (numbered 0 through 23)
+on a particular day (numbered 0 through 6)
+on a particular week (numbered 0 through 51)
+over the last ten years (numbered 0 through 9).
+The innermost dimension should be years, with the next being weeks, and so on.
+*/
+
+int westboundHollandTunnelTraffic[10][52][7][24];
