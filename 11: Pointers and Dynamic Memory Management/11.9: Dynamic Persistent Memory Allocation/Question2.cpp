7e435b4bde4e3845a17562a84f645057662cc621
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-11-22 11:14:08 -0600

    Create Question2.cpp

---
+++ 11: Pointers and Dynamic Memory Management/11.9: Dynamic Persistent Memory Allocation/Question2.cpp
@@ -0,0 +1,6 @@
+/*
+Declare a variable, bp, as a pointer to bool, dynamically allocate memory for a single bool value, assign the resulting pointer to bp and initialize the value to true.
+*/
+
+bool *bp;
+bp = new bool(true);
