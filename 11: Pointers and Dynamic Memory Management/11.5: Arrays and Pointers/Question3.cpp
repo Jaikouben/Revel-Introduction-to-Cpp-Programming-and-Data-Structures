31ea2e9b98351c9c38a8743befeb3d4ecd782309
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-11-22 11:09:51 -0600

    Create Question3.cpp

---
+++ 11: Pointers and Dynamic Memory Management/11.5: Arrays and Pointers/Question3.cpp
@@ -0,0 +1,6 @@
+/*
+Assume that ip has been declared to be a pointer to int and that enrollment has been declared to be an array of 20 elements.
+Write a statement that makes ip point to the first element in the array.
+*/
+
+ip = enrollment;
