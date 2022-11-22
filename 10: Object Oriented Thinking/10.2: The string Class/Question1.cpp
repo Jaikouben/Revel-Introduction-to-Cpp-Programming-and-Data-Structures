d2477cdce0116971c82b61f05d806143ecac6206
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-11-22 10:58:25 -0600

    Create Question1.cpp

---
+++ 10: Object Oriented Thinking/10.2: The string Class/Question1.cpp
@@ -0,0 +1,10 @@
+/*
+Assume that sentence is a variable of type string that has been assigned a value.
+Assume furthermore that this value is a string consisting of words separated by single space characters with a period at the end.
+For example: "This is a possible value of sentence."
+Assume that there is another variable declared, secondWord, also of type string.
+Write the statements needed so that the second word of the value of sentence is assigned to secondWord.
+So, if the value of sentence were "Broccoli is delicious." your code would assign the value "is" to secondWord.
+*/
+
+firstWord = sentence.substr(0, sentence.find(secondWord, 0));
