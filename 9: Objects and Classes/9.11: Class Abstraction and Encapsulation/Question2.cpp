b5ee4b85924ba31dddd5aaccf65e02beeac37ea9
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-11-22 10:47:25 -0600

    Create Question2.cpp

---
+++ 9: Objects and Classes/9.11: Class Abstraction and Encapsulation/Question2.cpp
@@ -0,0 +1,11 @@
+/*
+Write the implementation (.cpp file) of the Acc2 class of the previous exercise.
+The full specification of the class is:
+A data member named sum of type int.
+A constructor that accepts no parameters. The constructor initializes the data member sum to 0.
+A function named getSum that accepts no parameters and returns an int. getSum returns the value of sum.
+Don’t include the header file in this exercise, because REVEL assumes that the header and implementation are placed in the same file.
+*/
+
+Acc2::Acc2(){sum = 0;}
+int Acc2::getSum(){return sum;}
