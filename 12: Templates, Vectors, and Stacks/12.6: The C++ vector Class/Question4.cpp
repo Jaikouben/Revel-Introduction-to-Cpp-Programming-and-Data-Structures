30c054061b79b496c46fd958ef9d099cb22ac960
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-11-22 11:24:12 -0600

    Create Question4.cpp

---
+++ 12: Templates, Vectors, and Stacks/12.6: The C++ vector Class/Question4.cpp
@@ -0,0 +1,6 @@
+/*
+Assume that the vector arr has been declared. In addition, assume that VECTOR_SIZE has been defined to be an int that equals the number of elements in arr.
+Write a statement that assigns the next to last element of the vector to the variable x (x has already been declared).
+*/
+
+x = arr[VECTOR_SIZE - 2];
