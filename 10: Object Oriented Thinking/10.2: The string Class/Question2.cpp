1ca369688d357810aeac26a47264ec7dd8b55e6f
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-11-22 10:59:27 -0600

    Create Question2.cpp

---
+++ 10: Object Oriented Thinking/10.2: The string Class/Question2.cpp
@@ -0,0 +1,5 @@
+/*
+Assume that s is a string. Write an expression whose value is true if and only if the value of s would come between "mortgage" and "mortuary" in the dictionary.
+*/
+
+s > "mortgage" && s < "mortuary"
