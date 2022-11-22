5f851700c4f59f0961f10d575c2062801274be50
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-11-22 11:14:26 -0600

    Create Question3.cpp

---
+++ 11: Pointers and Dynamic Memory Management/11.9: Dynamic Persistent Memory Allocation/Question3.cpp
@@ -0,0 +1,6 @@
+/*
+The variable dp is to refer to an array of double. Assuming the int variable n has been assigned a value, declare dp appropriately, allocate an array of n double values and assign the resulting pointer to dp.
+*/
+
+double *dp;
+dp = new double[n];
