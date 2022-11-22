cf403a8e3072b4dbce4be428cad9aaf48c3fa2ca
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-11-22 10:42:09 -0600

    Create Question3.cpp

---
+++ 9: Objects and Classes/9.4: Constructors/Question3.cpp
@@ -0,0 +1,6 @@
+/*
+Objects of class Name are defined by supplying last name (string) and a first name (string) in this order.
+Declare an object of type Name named name and corresponding to a last name of "Black" and a first name of "Susan".
+*/
+
+Name name("Black","Susan");
