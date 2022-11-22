ffe1a96d8128ab76926ad8ad20308a901e7320ae
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-11-22 10:37:43 -0600

    Create Question1.cpp

---
+++ 9: Objects and Classes/9.3: Example: Defining Classes and Creating Objects/Question1.cpp
@@ -0,0 +1,6 @@
+/*
+Objects of the Calculator class require no additional information when created.
+Declare an object named calc of type Calculator using the no-arg constructor.
+*/
+
+Calculator calc;
