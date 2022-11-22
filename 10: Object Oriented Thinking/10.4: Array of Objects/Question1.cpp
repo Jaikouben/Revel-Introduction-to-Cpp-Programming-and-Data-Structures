b116b0479a60220e886c81b6924d3326c3a66fac
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-11-22 11:00:25 -0600

    Create Question1.cpp

---
+++ 10: Object Oriented Thinking/10.4: Array of Objects/Question1.cpp
@@ -0,0 +1,6 @@
+/*
+Assume a class Window with a constructor that accepts two int arguments: width and height (in that order).
+Declare an array named winarr, consisting of 3 Window objects, where the first element is an 80x20 window, the second a 10x10 window, and the third a 133x40 window.
+*/
+
+Window winarr[] = {Window(80,20), Window(10,10), Window(133,40)};
