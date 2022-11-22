d1dbead2bb53af96dd35dd250d0f8d111fcc6807
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-11-22 10:41:26 -0600

    Create Question1.cpp

---
+++ 9: Objects and Classes/9.4: Constructors/Question1.cpp
@@ -0,0 +1,6 @@
+/*
+Creating objects of the Currency class requires a name (string), a currency symbol (string), and the number of decimal places (int) usually associated with the currency (in that order).
+Declare an object named curr of type Currency corresponding to "US Dollar" with the symbol "$" and 2 decimal places.
+*/
+
+Currency curr ("US Dollar", "$", 2);
