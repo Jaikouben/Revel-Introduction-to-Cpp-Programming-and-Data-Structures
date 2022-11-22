0c4a4b0253d2f7046c7ca3a4c7676e3ead783df3
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-11-22 11:13:48 -0600

    Create Question1.cpp

---
+++ 11: Pointers and Dynamic Memory Management/11.9: Dynamic Persistent Memory Allocation/Question1.cpp
@@ -0,0 +1,5 @@
+/*
+Given the variable ip, already declared as a pointer to an int, write the code to dynamically allocate memory for a single int value, assign the resulting pointer to ip, and initialize the int value to 27.
+*/
+
+ip = new int(27);
