935392337f6c3525a88683cff66301cb6b1db344
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-11-22 10:40:24 -0600

    Create Question5.cpp

---
+++ 9: Objects and Classes/9.3: Example: Defining Classes and Creating Objects/Question5.cpp
@@ -0,0 +1,6 @@
+/*
+Objects of the Window class require a width(int) and a height(int) be specified (in that order) upon creation.
+Declare an object named window, of type Window, whose width and length are obtained from the (already declared and initialized) variables, w and l.
+*/
+
+Window window (w,l);
