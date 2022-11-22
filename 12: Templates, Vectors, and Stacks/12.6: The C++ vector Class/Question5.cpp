151e5e11770b6f4d90a113195e357c9866a921ba
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-11-22 11:24:34 -0600

    Create Question5.cpp

---
+++ 12: Templates, Vectors, and Stacks/12.6: The C++ vector Class/Question5.cpp
@@ -0,0 +1,6 @@
+/*
+Given that a vector of int named a has been declared with 12 elements and that the int variable k holds a value between 0 and 6.
+Assign 9 to the element just after a[k].
+*/
+
+a[k + 1] = 9;
