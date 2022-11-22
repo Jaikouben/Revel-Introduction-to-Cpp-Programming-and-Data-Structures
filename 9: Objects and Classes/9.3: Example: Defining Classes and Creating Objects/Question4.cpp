f2bc48d6d0ca5d2e836407f5b0356a16dc8a4812
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-11-22 10:40:00 -0600

    Create Question4.cpp

---
+++ 9: Objects and Classes/9.3: Example: Defining Classes and Creating Objects/Question4.cpp
@@ -0,0 +1,6 @@
+/*
+Objects of the Window class require a width(int) and a height(int) be specified (in that order) upon creation.
+Declare an object named window, of type Window, corresponding to an 80 x 20 window.
+*/
+
+Window window(80, 20);
